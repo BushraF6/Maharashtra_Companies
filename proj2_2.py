303c85bfdb8375c50d7e2d76860edfd052b82354
BushraFatima <138645454+BushraF6@users.noreply.github.com>
2023-07-26 22:59:44 +0530

    Added Comments.

--- proj2_2.py
+++ proj2_2.py
@@ -1,20 +1,25 @@
+""" Importing the required modules to solve the problem. """
+from matplotlib import pyplot as plt
 import mahamains
-import matplotlib.pyplot as plt
-com_maharashtra=mahamains.maharashtra_reader
-year={}
+
+com_maharashtra = mahamains.maharashtra_reader
+# Create dictionary to store no: of registrations per year.
+year = {}
+# Calculate no: of registrations per year.
 for company in com_maharashtra:
-    y=company['DATE_OF_REGISTRATION'][-2:]
-    if y!="NA":
-        if int(y)<23:
-            y='20'+y
+    y = company['DATE_OF_REGISTRATION'][-2:]
+    if y != "NA":
+        if int(y) < 23:
+            y = '20'+y
         else:
-            y='19'+y
-        if y in year.keys():
-            year[y]+=1
+            y = '19'+y
+        if y in year:
+            year[y] += 1
         else:
-            year[y]=1
+            year[y] = 1
+# Plotting the calculated data.
 plt.bar(list(year.keys()), list(year.values()))
 plt.title('Company Registration per Year')
 plt.xlabel('Year')
 plt.ylabel('Number of Companies Registered')
-plt.show() 
+plt.show()
