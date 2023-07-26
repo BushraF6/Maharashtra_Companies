4484c68bb8e97530fe7a80a1a2f443e6723960f1
BushraFatima <138645454+BushraF6@users.noreply.github.com>
2023-07-26 22:57:48 +0530

    Added Comments.

--- proj2_1.py
+++ proj2_1.py
@@ -1,14 +1,17 @@
+""" Importing the required modules to solve the problem. """
 from matplotlib import pyplot as plt
 import mahamains
 
-com_maharashtra= mahamains.maharashtra_reader 
-aut_capital= [] 
-for company in com_maharashtra:     
-    aut_capital.append(company['AUTHORIZED_CAP']) 
-aut_capital.sort() 
+com_maharashtra = mahamains.maharashtra_reader
+# Create list to store the authorized capital of companies.
+aut_capital = []
+# Appending the authorized capital values to aut_capital list.
+for company in com_maharashtra:
+    aut_capital.append(company['AUTHORIZED_CAP'])
+aut_capital.sort()
 
-plt.hist(aut_capital,bins =20) 
+plt.hist(aut_capital, bins=20)
 plt.xticks(ticks=['0', '100000', '1000000', '10000000', '100000000',
-             max(aut_capital)], labels=([min(aut_capital),
-             '<1L', '1L-10L', '10L-1CR', '1CR-10CR', '>10CR'])) 
-plt.show() 
+                  max(aut_capital)], labels=([min(aut_capital),
+                  '<1L', '1L-10L', '10L-1CR', '1CR-10CR', '>10CR']))
+plt.show()
