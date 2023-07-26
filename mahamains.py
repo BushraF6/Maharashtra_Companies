026732dccb6e293882fd91fdd0edd61ee7cbc326
BushraFatima <138645454+BushraF6@users.noreply.github.com>
2023-07-26 23:09:40 +0530

    Added Comments.

--- mahamains.py
+++ mahamains.py
@@ -1,8 +1,10 @@
+""" Importing csv module to extract data. """
 import csv
 
-maharashtra_file=open('Maharashtra.csv', encoding='unicode_escape')
-maharashtra_red=csv.DictReader(maharashtra_file)
-maharashtra_reader=list(maharashtra_red)
-
-districtfile=open('districts.csv', encoding='unicode_escape')
-district_reader=list(csv.DictReader(districtfile))
+# Opening file and extracting data from Maharashtra file.
+maharashtra_file = open('Maharashtra.csv', encoding='unicode_escape')
+maharashtra_red = csv.DictReader(maharashtra_file)
+maharashtra_reader = list(maharashtra_red)
+# Opening file and extracting data from District file.
+districtfile = open('districts.csv', encoding='unicode_escape')
+district_reader = list(csv.DictReader(districtfile))
